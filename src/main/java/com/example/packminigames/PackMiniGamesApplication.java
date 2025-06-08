6515ee1a20a1d8ce540d91b951d08e57690e6527
SeverynUA <cool.muzzy.gmaio.com@gmail.com>
2025-06-07 17:09:05 -0700

    Initial commit

---
+++ src/main/java/com/example/packminigames/PackMiniGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.packminigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackMiniGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackMiniGamesApplication.class, args);
+	}
+
+}
