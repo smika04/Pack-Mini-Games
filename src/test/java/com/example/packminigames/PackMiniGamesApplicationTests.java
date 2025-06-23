f1c9a6a60606783116c05630659313e6f61ce7d1
SeverynUA <cool.muzzy.gmaio.com@gmail.com>
2025-06-22 17:42:10 -0700

    update basic test

--- src/test/java/com/example/packminigames/PackMiniGamesApplicationTests.java
+++ src/test/java/com/example/packminigames/PackMiniGamesApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.packminigames;
 import com.example.packminigames.Mapping.CheckAllMappers;
 import com.example.packminigames.Profiles.CheckAllProfilesGroup;
 import com.example.packminigames.Profiles.CheckDatabaseConfiguration.AllDatabaseIntegrationTestsSuite;
+import com.example.packminigames.Service.DAO.AllServiceDaoIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
@@ -15,7 +16,8 @@ import org.springframework.test.context.ActiveProfiles;
 @SelectClasses({
         AllDatabaseIntegrationTestsSuite.class,
         CheckAllProfilesGroup.class,
-        CheckAllMappers.class
+        CheckAllMappers.class,
+        AllServiceDaoIntegrationTest.class
 })
 class PackMiniGamesApplicationTests {
 
